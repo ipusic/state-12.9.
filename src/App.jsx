d42cf677693b9169c67c99769b8df234cde0bb6b
IVANA_PUSIC <ipusic0212@gmail.com>
2023-09-13 17:06:48 +0200

    initial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+ import './App.css'
+ import { Component } from 'react'
+
+class App extends Component {
+  render () {
+    return (
+      <>
+        <h1>state</h1>
+      </>
+    )
+  }
+
+}
+
+export default App
